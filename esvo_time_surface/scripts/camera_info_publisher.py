c690ba6c7eb44127205663e8b8b2969c2a355923
Tom Schammo <tom@tomschammo.com>
2026-04-07 19:05:52 +0200

    style: Autoformat

--- esvo_time_surface/scripts/camera_info_publisher.py
+++ esvo_time_surface/scripts/camera_info_publisher.py
@@ -20,6 +20,7 @@ from sensor_msgs.msg import CameraInfo
 
 
 class CameraInfoPublisher(Node):
+
     def __init__(self):
         super().__init__('camera_info_publisher')
 
@@ -31,11 +32,16 @@ class CameraInfoPublisher(Node):
         self.declare_parameter('right_topic', '/evk/right/camera_info')
 
         # Get parameters
-        calib_dir = self.get_parameter('calib_dir').get_parameter_value().string_value
-        frame_id = self.get_parameter('frame_id').get_parameter_value().string_value
-        publish_rate = self.get_parameter('publish_rate').get_parameter_value().double_value
-        left_topic = self.get_parameter('left_topic').get_parameter_value().string_value
-        right_topic = self.get_parameter('right_topic').get_parameter_value().string_value
+        calib_dir = self.get_parameter(
+            'calib_dir').get_parameter_value().string_value
+        frame_id = self.get_parameter(
+            'frame_id').get_parameter_value().string_value
+        publish_rate = self.get_parameter(
+            'publish_rate').get_parameter_value().double_value
+        left_topic = self.get_parameter(
+            'left_topic').get_parameter_value().string_value
+        right_topic = self.get_parameter(
+            'right_topic').get_parameter_value().string_value
 
         if not calib_dir:
             self.get_logger().error('calib_dir parameter is required!')
@@ -96,15 +102,16 @@ class CameraInfoPublisher(Node):
         msg.d = [float(x) for x in d_data[:5]]
 
         # Camera matrix (K) - 3x3 -> 9 elements
-        k_data = calib.get('camera_matrix', {}).get('data', [0]*9)
+        k_data = calib.get('camera_matrix', {}).get('data', [0] * 9)
         msg.k = [float(x) for x in k_data[:9]]
 
         # Rectification matrix (R) - 3x3 -> 9 elements
-        r_data = calib.get('rectification_matrix', {}).get('data', [1,0,0, 0,1,0, 0,0,1])
+        r_data = calib.get('rectification_matrix',
+                           {}).get('data', [1, 0, 0, 0, 1, 0, 0, 0, 1])
         msg.r = [float(x) for x in r_data[:9]]
 
         # Projection matrix (P) - 3x4 -> 12 elements
-        p_data = calib.get('projection_matrix', {}).get('data', [0]*12)
+        p_data = calib.get('projection_matrix', {}).get('data', [0] * 12)
         msg.p = [float(x) for x in p_data[:12]]
 
         self.get_logger().info(
